e5beb0bb20e18aaf572e214018a7018abb010eef
David Zhang <dzhang21@gmail.com>
2014-04-18 11:15:53 -0700

    include diphthong gen in decoding

--- script3.sh
+++ script3.sh
@@ -19,12 +19,8 @@ clang++ -stdlib=libc++ -std=c++11 PhonemeToLetterNSV_FST.cc CypherReader.cc -o 4
 carmel --train-cascade -HJ -X .99999 -! 20 -: $cipher_file syllables.wfsa syll_to_type.fst onset_maximizing_filter.fst type_to_more_phonemes.fst diphthong_generator2.fst phoneme_to_letter_nsv.fst
 # carmel --project-right --project-identity-fsa -HJ syllables.wfsa > syllables.wfsa.noe 
 awk 'NF>0' $cipher_file > cipher.data.noe 
-# Take 2's trained machine and remove it's input. Why is this needed for the
+# Remove last machine's input. Why is this needed for the
 # last machine on the left? Answer: This makes x:a to a:a, so you drop any x
 # after this a. The output we want to see is the "a".
-
-# carmel --project-right --project-identity-fsa -HJ syll_to_type.fst.trained > syll_to_type.fst.noe 
-# cat cipher.data.noe | carmel -qbsriWEIk 1 syll_to_type.fst.noe type_to_more_phonemes.fst.trained phoneme_to_letter_nsv.fst.trained
-
 carmel --project-right --project-identity-fsa -HJ type_to_more_phonemes.fst.trained > type_to_more_phonemes.fst.noe 
-cat cipher.data.noe | carmel -qbsriWEIk 1 type_to_more_phonemes.fst.noe phoneme_to_letter_nsv.fst.trained
+cat cipher.data.noe | carmel -qbsriWEIk 1 type_to_more_phonemes.fst.noe diphthong_generator2.fst.trained phoneme_to_letter_nsv.fst.trained
