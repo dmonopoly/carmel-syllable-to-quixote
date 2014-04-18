0e7908b2950f95322905e70a65b1ed2ecdeb84f0
David Zhang <dzhang21@gmail.com>
2014-04-18 11:09:48 -0700

    Included onset maximizer, added CypherReader (more robust) - good results

--- DiphthongGenerator2.cc
+++ DiphthongGenerator2.cc
@@ -3,11 +3,9 @@
 #include <sstream>
 #include <vector>
 
-#include "CypherReader.h"
-
 using namespace std;
 
-const string FST_FILE = "phoneme_to_letter_nsv.fst";
+const string FST_FILE = "diphthong_generator2.fst";
 const string EMPTY = "*e*";
 
 void WriteLine(ofstream &fout, const string &node1, const string &node2,
@@ -47,37 +45,20 @@ void WriteLine(ofstream &fout, const string &node1, const string &node2,
 }
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    cerr << "Usage: ./<exec> <cipher file>" << endl;
+  if (argc != 1) {
+    cerr << "Usage: ./<exec> " << endl;
     return 0;
   }
-  vector<string> inputs{"S", "N", "V"};
-  string filename_for_cypher = argv[1];
-  vector<string> observed_data;
-  set<string> letters;
-  bool got_obs_data = CypherReader::GetObservedData(filename_for_cypher,
-                                                    &observed_data,
-                                                    &letters);
-  if (!got_obs_data) {
-    cerr << "Error getting observed data." << endl;
-    return 1;
-  }
-//   vector<string> tag_list{"B", "D", "G", "J", "L", "T", "a", "b", "d", "e", "f",
-//     "g", "i", "k", "l", "m", "n", "o", "p", "r", "rr", "s", "t", "tS", "u",
-//     "h"};
   // Begin writing the FST.
   ofstream fout;
   fout.open(FST_FILE.c_str());
-  string only_node = "0";
-  fout << only_node << endl;
-  // Remove the _ that was read in!
-  letters.erase("_");
-  WriteLine(fout, only_node, only_node, "_", "_");
-  for (string in : inputs) {
-    for (string letter : letters) {
-      WriteLine(fout, only_node, only_node, in, letter);
-    }
-  }
+  string start_node = "0";
+  fout << start_node << endl;
+  WriteLine(fout, start_node, start_node, "_", "_", 1, "!");
+  WriteLine(fout, start_node, start_node, "S", "S", 1, "!");
+  WriteLine(fout, start_node, start_node, "N", "N", 1, "!");
+  WriteLine(fout, start_node, start_node, "V", "V", .5, "");
+  WriteLine(fout, start_node, start_node, "V", "VV", .5, "");
   fout.close();
   cout << "Written to " << FST_FILE << endl;
   return 0;
--- script3.sh
+++ script3.sh
@@ -1,4 +1,8 @@
 # Like script2, except now we deal with N, S, and V - the end of the 06 paper!
+# cipher_file='Ciphers/spanish.written_short.quoted.txt'
+cipher_file='Ciphers/spanish.written_one_third.txt'
+# cipher_file='Ciphers/spanish.written.quoted.txt'
+
 clang++ -stdlib=libc++ -std=c++11 SyllableGenerator.cc -o 1.out
 ./1.out
 clang++ -stdlib=libc++ -std=c++11 SyllableToTypeFST.cc -o 2.out
@@ -7,17 +11,12 @@ clang++ -stdlib=libc++ -std=c++11 OnsetMaximizingFilter.cc -o 2a.out
 ./2a.out
 clang++ -stdlib=libc++ -std=c++11 TypeToMorePhonemesFST.cc -o 3.out
 ./3.out
-# clang++ -stdlib=libc++ -std=c++11 TypeToPhonemeFST.cc -o 3.out
-# ./3.out
+clang++ -stdlib=libc++ -std=c++11 DiphthongGenerator2.cc -o 3a.out
+./3a.out
+clang++ -stdlib=libc++ -std=c++11 PhonemeToLetterNSV_FST.cc CypherReader.cc -o 4.out
+./4.out $cipher_file
 
-clang++ -stdlib=libc++ -std=c++11 PhonemeToLetterNSV_FST.cc -o 4.out
-./4.out
-# clang++ -stdlib=libc++ -std=c++11 PhonemeToLetterFST.cc -o 4.out
-# ./4.out
-
-# cipher_file='Ciphers/spanish.written_short.quoted.txt'
-cipher_file='Ciphers/spanish.written_one_third.txt'
-carmel --train-cascade -HJ -X .99999 -! 30 -: $cipher_file syllables.wfsa syll_to_type.fst onset_maximizing_filter.fst type_to_more_phonemes.fst phoneme_to_letter_nsv.fst
+carmel --train-cascade -HJ -X .99999 -! 20 -: $cipher_file syllables.wfsa syll_to_type.fst onset_maximizing_filter.fst type_to_more_phonemes.fst diphthong_generator2.fst phoneme_to_letter_nsv.fst
 # carmel --project-right --project-identity-fsa -HJ syllables.wfsa > syllables.wfsa.noe 
 awk 'NF>0' $cipher_file > cipher.data.noe 
 # Take 2's trained machine and remove it's input. Why is this needed for the
