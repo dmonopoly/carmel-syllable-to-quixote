ac0ebff45c76b4b83388f42e91055f2bcc5fb0a0
David Zhang <dzhang21@gmail.com>
2014-05-04 12:13:51 -0700

    bigram sparse results - iffy

--- script4.sh
+++ script4.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#PBS -l walltime=1:00:00
-# cd $PBS_O_WORKDIR
+#PBS -l walltime=7:00:00
+cd $PBS_O_WORKDIR
 # After 1-3a, add 4p (4 prime): phoneme class to phoneme, and 5p: phoneme to letter
-cipher_file='Ciphers/spanish.written_short.quoted.txt'
+# cipher_file='Ciphers/spanish.written_short.quoted.txt'
 # cipher_file='Ciphers/spanish.written_one_third.txt'
-# cipher_file='Ciphers/spanish.written.quoted.txt'
+cipher_file='Ciphers/spanish.written.quoted.txt'
 
 clang++ SyllableGenerator.cc -o 1.out
 ./1.out
@@ -21,7 +21,7 @@ clang++ PhonemeClassToPhonemeFST.cc -o 4p.out
 clang++ PhonemeToLetterFST.cc CypherReader.cc -o 5p.out
 ./5p.out $cipher_file
 
-carmel --train-cascade -HJ -X .99999 $cipher_file syllables.wfsa syll_to_type.fst onset_maximizing_filter.fst type_to_more_phoneme_classes.fst diphthong_generator2.fst phoneme_class_to_phoneme.fst phoneme_to_letter.fst
+carmel --train-cascade -HJ -X .99999 -! 10 $cipher_file syllables.wfsa syll_to_type.fst onset_maximizing_filter.fst type_to_more_phoneme_classes.fst diphthong_generator2.fst phoneme_class_to_phoneme.fst phoneme_to_letter.fst
 # carmel --project-right --project-identity-fsa -HJ syllables.wfsa > syllables.wfsa.noe 
 awk 'NF>0' $cipher_file > cipher.data.noe 
 # Remove last machine's input. Why is this needed for the
