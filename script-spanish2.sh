677b7f0ee84bf1d70c07eef0e9633d328223abc1
David Zhang <dzhang21@gmail.com>
2014-05-12 08:17:04 -0700

    known r quick test

---
+++ script-spanish2.sh
@@ -0,0 +1,22 @@
+#PBS -l walltime=10:00:00
+# Part of the final 3rd task that only uses ~3 machines: syllable generator ->
+# syllable to phonemes (this file) -> phoneme to letter
+# cd $PBS_O_WORKDIR
+# cipher_file='Ciphers/spanish.written_short.quoted.txt'
+# cipher_file='Ciphers/spanish.written_one_third.txt'
+cipher_file='Ciphers/spanish.written.quoted.txt'
+
+clang++ SyllableGenerator.cc -o 1.out
+./1.out
+clang++ SyllableToPhonemesFST.cc -o 2.out
+./2.out
+# assumes already there
+# clang++ PhonemeToLetterFST.cc CypherReader.cc -o 3.out
+# ./3.out $cipher_file
+
+carmel --train-cascade -HJ -X .99999 -! 1 $cipher_file syllables.wfsa syll_to_phonemes.fst phoneme_to_letter_known_r.fst
+# carmel --project-right --project-identity-fsa -HJ syllables.wfsa > syllables.wfsa.noe 
+awk 'NF>0' $cipher_file > cipher.data.noe 
+# carmel --project-right --project-identity-fsa -HJ type_to_more_phonemes.fst.trained > type_to_more_phonemes.fst.noe 
+cat cipher.data.noe | carmel -qbsriWEIk 1  phoneme_to_letter_known_r.fst.trained
+
